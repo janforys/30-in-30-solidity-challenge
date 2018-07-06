333c30ffbde84715bb87ba6a13ff378bb6568c0f
Jan <janforys@wp.pl>
2018-07-06 11:52:22 +0200

    Finishing Account

--- 02_account/Account.sol
+++ 02_account/Account.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.24;
 
 /**
 * @title Account
@@ -8,17 +8,46 @@ pragma solidity ^0.4.23;
 
 contract Account {
 
-    address contractAddress;
-    address ownerAddress;
-    address senderAddress;
-    uint accountBalance;
-    uint ownerBalance;
-    uint senderBalance;
+    // State variables
+    address owner;
+
+    constructor () public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Only owner can check his own account balance.");
+        _;
+    }
 
     // function that returns contract address
+    function getContractAddress() public view returns (address) {
+        return this;
+    }
+
     // function that returns address of contract's owner
+    function getOwnerAddress() public view returns (address) {
+        return owner;
+    }
+
     // function that returns address of sender (i.e. person who is calling a function)
-    // function that returns account balance
+    function getSenderAddress() public view returns (address) {
+        return msg.sender;
+    }
+
+    // function that returns contract balance
+    function getContractBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
     // function that returns balance of contract's owner account (and only owner can call this function)
+    function getOwnerBalance() public view onlyOwner returns (uint) {
+        return owner.balance;
+    }   
+    
     // function that returns balance of sender account (i.e. person who is calling a function)
+    function getSenderBalance() public view returns (uint) {
+        return msg.sender.balance;
+    }   
+
 }
