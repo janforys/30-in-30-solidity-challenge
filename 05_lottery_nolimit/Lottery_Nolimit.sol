20435421e73e96ca4d338c079fe15aafaa312664
Jan <janforys@wp.pl>
2018-07-16 12:09:56 +0200

    Lottery with no limit

---
+++ 05_lottery_nolimit/Lottery_Nolimit.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.4.24;
+
+/**
+* @title Lottery NoLimit
+* @author Jan Foryś
+* @notice A Lottery for no participants limit.
+*/
+
+contract Lottery_Nolimit {
+
+    address[] participants;   // no participants limit
+    address owner;
+    uint Nonce = 0;
+
+    modifier isOwner() {
+        require(msg.sender == owner, "Only owner can do that");
+        _;
+    }
+
+    constructor() public {
+        // contract owner can participate
+        owner = msg.sender;
+    }
+
+    // User can participate in lottery more than 1 time
+    function join() public payable {
+        // pay 0.1 Ether to participate
+        require(msg.value == 0.1 ether, "Must send 0.1 Eth");
+        participants.push(msg.sender);
+    }
+       
+    // Contract owner decide when to draw a winner
+    function drawWinner() public isOwner {
+        require(participants.length > 0, "No participants in the Lottery");
+        // winner gets all funds
+        participants[_randomNumber(participants.length)].transfer(address(this).balance);
+        delete participants;
+    }
+
+    function _randomNumber(uint _limit) private returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(msg.sender, Nonce))) % _limit;
+        Nonce++;
+        return rand;
+    }
+}
