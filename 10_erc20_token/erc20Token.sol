21fcf5e50c6acaa184ee3e4bca70e6d7b027e168
Jan <janforys@wp.pl>
2018-07-31 15:05:31 +0200

    Token ERC20

---
+++ 10_erc20_token/erc20Token.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.4.24;
+
+/**
+* @title Token ERC20
+* @author Jan Foryś
+* @notice Contract with Token of ERC20 Standard.
+*/
+
+contract erc20Token {
+
+    string name;    
+    uint8 decimals;    // decimals to show
+    string symbol;    // token ID
+    uint totalSupply;  
+    uint constant private MAX_UINT = 2**256 - 1;
+
+    mapping (address => uint) public balances;
+    mapping (address => mapping  (address => uint)) allowed;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    constructor(
+        uint _initialAmount,
+        string _tokenName,   
+        uint8 _decimalUnits,    
+        string _tokenSymbol   
+    ) public {
+        balances[msg.sender] = _initialAmount;
+        totalSupply = _initialAmount;
+        name = _tokenName;
+        decimals = _decimalUnits;
+        symbol = _tokenSymbol;
+    }
+
+    function transfer(address _to, uint _value) public returns (bool success) {
+        require(balances[msg.sender] >= _value);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
+        uint allowance = allowed[_from][msg.sender];
+        require(balances[_from] >= _value && allowance >= _value);
+        balances[_to] += _value;
+        balances[_from] -= _value;
+        if (allowance < MAX_UINT) {
+            allowed[_from][msg.sender] -= _value;
+        }
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+
+    /// Returns the account balance of another account with address _owner.
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }   
+
+}
