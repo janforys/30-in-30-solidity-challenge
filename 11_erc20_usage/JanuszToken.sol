bf5ac161d35f9080073748e4253ca2bd8d4c73e7
Jan <janforys@wp.pl>
2018-08-01 12:12:58 +0200

    ERC20 Usage

---
+++ 11_erc20_usage/JanuszToken.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.24;
+
+import "./StandardToken.sol";
+
+/**
+* @title Janusz Token
+* @author Jan Foryś
+* @notice This contract shows us usage of ERC20 Standard for our own Token.
+*/
+
+contract JanuszToken is StandardToken {
+
+    string public constant name = "JanuszToken";
+    string public constant symbol = "JSZ";
+    uint8 public constant decimals = 10;
+    uint public constant initialSupply = 666 * (10 ** uint(decimals));
+
+    constructor() public {
+        balances[msg.sender] = initialSupply;
+        totalSupply = initialSupply;
+    }
+
+}
